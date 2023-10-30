a00c6e22a8a26d7508551f6a6f564b5319fcdf93
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-10-30 12:23:50 -0700

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,11 @@ def teardown_appcontext(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def handle_404(error):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
