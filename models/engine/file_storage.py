71892187ad2d568c6e6923719b961ffd94d5d0eb
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-30 17:27:24 -0700

    Update file_storage.py file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,16 +73,12 @@ class FileStorage:
         """
         Returns the object based on the class and its ID, or None if not found
         """
-        key = "{}.{}".format(cls.__name__, id)
-        return self.__objects.get(key)
+        if cls and id:
+            fetch_obj = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch_obj)
+        return None
 
     def count(self, cls=None):
         """Count the number of objects in storage"""
-        if cls:
-            count = 0
-            for obj in self.__objects.values():
-                if type(obj) == cls:
-                    count += 1
-            return count
-        else:
-            return len(self.__objects)
+        return (len(self.all(cls)))
